d33bbff6a99b29f4d277c73f9d548d9f3bc0f156
heydar <heydar.mahmoodi75@gmail.com>
2020-03-17 18:14:05 -0400

    Name and FName Example

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,8 +2,11 @@
 #include<QDebug>
 int main()
 {
-    std::string description("Oops");
-    description="Oops!";
-    std::cout << description <<std::endl;
+    std::string name,fname;
+    std::cout<<"Inser your FirsName and SecondName: ";
+    std::cin>>name>>fname;
+    std::string completeName {"Name: "+name+"   FamilyName: "+fname };
+    std::cout<<completeName;
+    std::cout<<std::endl<<"Hi "<<name<<" "<<fname<<std::endl;
     return 0;
 }
