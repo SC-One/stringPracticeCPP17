d1c44db4f3ae49265cf34dfea0cd28c1833df533
heydar <heydar.mahmoodi75@gmail.com>
2020-03-18 03:02:49 -0400

    to upper with auto loop and with normal loop

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,10 +2,22 @@
 #include<QDebug>
 int main()
 {
-    std::string one {"1"};
-    short int correct {2};
-    std::cout<<(one + std::to_string(correct))<<std::endl;
-    one.append("0 ==> TrueFalse");
-    std::cout<<one<<std::endl;
+    std::string sentence {"Hi guys , I'm here now!"};
+    std::cout<<sentence<<std::endl;
+    size_t sizeLen {(sizeof (sentence)/sizeof (sentence[0]))};
+    for(size_t i=0;i< sizeLen;i++)
+        sentence[i]=std::toupper(sentence[i]);
+    std::cout<<sentence<<std::endl;
+
+    for(size_t i=0;i< sizeLen;i++)
+        sentence[i]=std::tolower(sentence[i]);
+    std::cout<<sentence<<std::endl;
+
+
+    /////////////////////////////////////////////
+    sentence = "Hi guys , I'm here now!" ;
+    for(auto &ch:sentence)
+        ch=std::toupper(ch);
+    std::cout<<sentence<<std::endl;
     return 0;
 }
