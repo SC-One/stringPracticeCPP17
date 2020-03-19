28dd99dce76626a61201d8b1f0ac4072dd81103c
heydar <heydar.mahmoodi75@gmail.com>
2020-03-19 09:08:42 -0400

    find substr in strings with function find()

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,32 +1,19 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
 int main()
 {
-    std::string text1{"Nope"};
-    std::string text2{"Yeap"};
-    std::string text3{"Nope"};
-    std::cout<<text1<<"<>"<<text2<<"    "<<text1.compare(text2)<<std::endl;         // N<Y          ==> negative
-    std::cout<<text2<<"<>"<<text1<<"    "<<text2.compare(text1)<<std::endl;         // Y>N          ==> Positive
-    std::cout<<text3<<"<>"<<text1<<"    "<<text3.compare(text1)<<std::endl;         // Nope==Nope   ==> Zero
+    std::string sentence {"His son has good brain."};
+    std::string word1 {"good"};
+    std::string word2 {"had"};
+    std::cout<<sentence.find(word1)<<std::endl;
+    std::cout<<sentence.find(word2)<<std::endl;
+    std::cout<<sentence.find("His")<<std::endl;
+    std::cout<<sentence.find("his")<<std::endl;
+    std::cout<<sentence.find('x')<<std::endl;
 
-
-    std::string word1{"A jackhammer"};
-    std::string word2{"jack"};
-    int result{word1.compare(2,word2.length(),word2)};
-    if(result==0)
-    {
-        std::cout<<"word1("<<word1<<") contains word2("<<word2<<")"<<std::endl;
-    }
-
-
-    std::string text{"Hamid has brown hair."};
-    std::string word{"brown"};
-    for(size_t i=0;i<(text.length()-word.length()+1);i++)
-    {
-        if(text.compare(i,word.length(),word)==0)
-        {
-            std::cout<<"atIndex["<<i<<"]: text("<<text<<") contains ("<<word<<")"<<std::endl;
-        }
-    }
+    // what's std::string::npos
+    if(sentence.find(word2) == std::string::npos)
+        std::cout<<"Not found!"<<std::endl;
     return 0;
 }
