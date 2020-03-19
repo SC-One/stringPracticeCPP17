23b3250e779b28947bd601629003466a88507ca0
heydar <heydar.mahmoodi75@gmail.com>
2020-03-19 06:30:40 -0400

    String names and Selection sort them <vector>

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,10 +1,35 @@
 #include<iostream>
-#include<QDebug>
+#include<string>
+#include<vector>
+
 int main()
 {
-    std::string phrase {"The higher the fewer."};
-    std::string word1 {phrase.substr(4, 6)};        // "higher"
-    std::string word2 {phrase.substr(4, 100)};      // "higher the fewer."
-    std::string word {phrase.substr(4)};            // "higher the fewr."
+    std::vector<std::string> list;
+    char ch{'y'};
+    do
+    {
+        std::string movaghat;
+        std::cout<<"Inputer the FullName: ";
+        std::cin>>movaghat;
+        list.push_back(movaghat);
+        std::cout<<"do you want insert more FullNames(y,n): ";
+        std::cin>>ch;
+    }while(std::tolower(ch)=='y');
+    // selectionSort
+    for(std::vector<std::string>::iterator it=list.begin(); it<(list.end()-1);it++)
+    {
+        for(std::vector<std::string>::iterator jt=it+1; jt<(list.end());++jt)
+        {
+            if(*jt < *it)
+            {
+                it->swap(*jt);
+            }
+        }
+    }
+    for(std::vector<std::string>::iterator it=list.begin(); it<list.end();it++)
+    {
+        std::cout<<*it<<std::endl;
+    }
+    std::cout<<"===============>Ended !"<<std::endl;
     return 0;
 }
