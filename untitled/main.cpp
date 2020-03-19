b26273f1eb7d9e553252c8e6586e4e36da47dc8e
heydar <heydar.mahmoodi75@gmail.com>
2020-03-19 14:57:23 -0400

    replace a string within a substring in text

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -3,34 +3,12 @@
 #include<vector>
 int main()
 {
-    std::vector<std::string> words;
-    std::string text;
-    std::cout<<"Insert a text that terminated by #: "<<std::endl;
-    std::getline(std::cin,text,'#');
-    const std::string seprators{" ,;:.\"!?'\n"};
-    size_t start {text.find_first_not_of(seprators)};
-    size_t end {text.find_first_of(seprators,start+1)};
-    size_t maxLength{0};
-    while(start!=std::string::npos)
-    {
-        if(end==std::string::npos)
-            end=text.length();
-        std::string help{text.substr(start,end-start)};
-        if(maxLength<help.length())
-            maxLength=help.length();
-        words.push_back(help);
-        start=text.find_first_not_of(seprators,end+1);
-        end=text.find_first_of(seprators,start+1);
-    }
-    size_t counter{0};
-    for(auto &wd:words)
-    {
-        size_t l=wd.length();
-        wd.insert(l,(maxLength-l+2),' ');
-        std::cout<<wd;
-        if((++counter%5)==0)
-            std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
+    std::string text{"Hamid has brown hair."};
+    std::string word{"green"};
+    const std::string separators {" ,;:.\"!'\n"};
+    size_t start{text.find("brown")};
+    size_t end{text.find_first_of(separators,start+1)};
+    text.replace(start,end-start,word);
+    std::cout<<text<<std::endl;
     return 0;
 }
