3516f4c535def0c8a1afc3fa9aad85ecf4ddf728
heydar <heydar.mahmoodi75@gmail.com>
2020-03-20 12:21:52 -0400

    Unicode and u32string , u16strings

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <iomanip>
-#include <vector>
 int main()
 {
-    std::vector<double> nums;
-    std::cout<<"Enter how many numbers you will insert? ";
-    size_t n;
-    std::cin>>n;
-    for (size_t i=0;i<n;i++)
-    {
-        double numInserted;
-        std::cin>>numInserted;
-        nums.push_back(numInserted);
-    }
-    std::stringstream streamerOfStrings;
-    for(size_t i=0;i<n;i++)
-    {
-        streamerOfStrings<<std::setprecision(4)<<std::setw(8)
-                        <<std::left<<nums.at(i);
-        if(i%4==0 && i!=0)
-            streamerOfStrings<<std::endl;
-    }
-    std::string textOfDoubles;
-    textOfDoubles.assign(streamerOfStrings.str());
-    std::cout<<textOfDoubles<<std::endl;
+
+    std::u16string one{u"one of you come"};
+    std::u32string two{U"both of you come"};
+
+    // @@Note@@
+    /*
+     * u16string and u32string have same function as string.
+     * so you can use find,erase,clear,replace,substr and etc.
+     */
+
+    std::string s(u8"字符串"); // UTF-8 encoding of the Chinese word for "string"
+    std::cout << s.length()<<"\t"<<s<<std::endl;   // Length: 9 code units!
+
+
+
+    // you can see ICU project to global unicode
     return 0;
 }
