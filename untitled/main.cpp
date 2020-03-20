e4a98889dc33443f6f9bbddf1936890739e0f3ed
heydar <heydar.mahmoodi75@gmail.com>
2020-03-20 05:28:47 -0400

    erase() function in strings and clear() showed

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,28 +2,23 @@
 #include <string>
 int main()
 {
-    std::string text,wordToChange,replaceWord;
-    std::cout<<"Insert a text that will terminated by *:\n";
-    std::getline(std::cin,text,'*');
-    std::cout<<"Insert the word to be changed: \n";
-    std::cin>>wordToChange;
-    std::cout<<"Insert the replacement string: \n";
-    std::cin>>replaceWord;
-    size_t wordToChangeLength{wordToChange.length()};
-    for(size_t i{0};i<(text.length()-wordToChange.length()+1);)
+    std::string text{"I never love you"};
+    std::string textEliminate{"never "};
+    size_t start{text.find(textEliminate)};
+    if(start!=std::string::npos)
     {
-        size_t foundIndex{text.find(wordToChange,i)};
-        if(foundIndex!=std::string::npos)
-        {
-            text.replace(foundIndex,wordToChangeLength,replaceWord);
-            i+=(foundIndex+wordToChangeLength);
-        }
-        else
-        {
-            break;
-        }
+        text.erase(start,textEliminate.length());
     }
-    std::cout<<":::::::::::::::::::::::::::::::"<<std::endl;
     std::cout<<text<<std::endl;
+
+    text.erase(2);
+    std::cout<<text<<std::endl;
+
+    text.erase();
+    text.append("I love God.");
+    std::cout<<text<<std::endl;
+    text.clear();
+    std::cout<<text<<std::endl;
+
     return 0;
 }
