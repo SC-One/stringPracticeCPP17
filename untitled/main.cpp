a873a5e460a4e0ae1d1c2ac90378b303ea1d2fc7
heydar <heydar.mahmoodi75@gmail.com>
2020-03-20 10:40:29 -0400

    used <sstream>Header to use StreamString and convert it to strings to avoid errorprone

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
 int main()
 {
-    /*
-     * std::stoi()          type integer
-     * std::stol()          type long
-     * std::stoll()         longlong
-     * std::stoul()         unsignedlong
-     * std::stoull()        unsignedlonglong
-     * std::stof()          float
-     * std::stod()          double
-     * std::stold()         longdouble
-     */
-
-    std::string s{ "1917" };
-    int i{ std::stoi(s) };      // i == 1917
-    std::cout<<i<<std::endl;
-    s.assign("1.234");
-    double d{std::stod(s)};
-    d/=10;
-    std::cout<<d<<std::endl;
+    std::vector<double> nums;
+    std::cout<<"Enter how many numbers you will insert? ";
+    size_t n;
+    std::cin>>n;
+    for (size_t i=0;i<n;i++)
+    {
+        double numInserted;
+        std::cin>>numInserted;
+        nums.push_back(numInserted);
+    }
+    std::stringstream streamerOfStrings;
+    for(size_t i=0;i<n;i++)
+    {
+        streamerOfStrings<<std::setprecision(4)<<std::setw(8)
+                        <<std::left<<nums.at(i);
+        if(i%4==0 && i!=0)
+            streamerOfStrings<<std::endl;
+    }
+    std::string textOfDoubles;
+    textOfDoubles.assign(streamerOfStrings.str());
+    std::cout<<textOfDoubles<<std::endl;
     return 0;
 }
