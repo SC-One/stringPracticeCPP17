c4adb8bd40b38197dca49500bb8f1cd07dd9f609
heydar <heydar.mahmoodi75@gmail.com>
2020-03-20 05:06:15 -0400

    Example to replace a word in a text

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <string>
-#include<vector>
 int main()
 {
-    std::string text{"Hamid has brown hair."};
-    std::string word{"green"};
-    const std::string separators {" ,;:.\"!'\n"};
-    size_t start{text.find("brown")};
-    size_t end{text.find_first_of(separators,start+1)};
-    text.replace(start,end-start,word);
+    std::string text,wordToChange,replaceWord;
+    std::cout<<"Insert a text that will terminated by *:\n";
+    std::getline(std::cin,text,'*');
+    std::cout<<"Insert the word to be changed: \n";
+    std::cin>>wordToChange;
+    std::cout<<"Insert the replacement string: \n";
+    std::cin>>replaceWord;
+    size_t wordToChangeLength{wordToChange.length()};
+    for(size_t i{0};i<(text.length()-wordToChange.length()+1);)
+    {
+        size_t foundIndex{text.find(wordToChange,i)};
+        if(foundIndex!=std::string::npos)
+        {
+            text.replace(foundIndex,wordToChangeLength,replaceWord);
+            i+=(foundIndex+wordToChangeLength);
+        }
+        else
+        {
+            break;
+        }
+    }
+    std::cout<<":::::::::::::::::::::::::::::::"<<std::endl;
     std::cout<<text<<std::endl;
     return 0;
 }
