e98642d0dab65964b22fc1668629370b39b39ddc
heydar <heydar.mahmoodi75@gmail.com>
2020-03-20 12:54:05 -0400

    Raw String Litterals  and Delimiters

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,21 +2,39 @@
 #include <string>
 int main()
 {
-
-    std::u16string one{u"one of you come"};
-    std::u32string two{U"both of you come"};
-
-    // @@Note@@
-    /*
-     * u16string and u32string have same function as string.
-     * so you can use find,erase,clear,replace,substr and etc.
-     */
-
-    std::string s(u8"字符串"); // UTF-8 encoding of the Chinese word for "string"
-    std::cout << s.length()<<"\t"<<s<<std::endl;   // Length: 9 code units!
+    auto one{ "The \"\\\\\" escape sequence is a backslash character, \\." };
+    std::cout<<one<<std::endl;
+    auto two{ "C:\\ProgramData\\MyCompany\\MySoftware\\MyFile.ext" };
+    std::cout<<two<<std::endl;
+    auto three{ L"First line.\nSecond line.\nThird line.\nThe end." };
+    std::cout<<three<<std::endl;
 
 
+    // @@@@@@@@@@@@@@@@@@@@@@@@ now added Raw litteral ===================> {R"(...)"};
+    auto escape{ R"(The "\\" escape sequence is a backslash character, \.)" };
+    std::cout<<escape<<std::endl;
+    auto path{ R"(C:\ProgramData\MyCompany\MySoftware\MyFile.ext)" };
+    std::cout<<path<<std::endl;
+    auto text{ LR"(First line.Second line.Third line.The end.)" };
+    std::cout<<text<<std::endl;
 
-    // you can see ICU project to global unicode
+
+    auto diff{ R"(The \"\\\\\" escape sequence is a backslash character, \\.)" };
+    std::cout<<diff<<std::endl;
+
+
+
+    std::string rawInString{R"(First line.\nSecond line.\nThird line.\nThe end.)"};
+    std::cout<<rawInString<<std::endl;
+    std::string inString{"(First line.\nSecond line.\nThird line.\nThe end.)"};
+    std::cout<<inString<<std::endl;
+
+    // if followin a " character to ) ==> R"(The answer is "(a - b)" not "(c - d)")"// Error!
+    // we should use custome delimiters!
+    // R"Fa-la-la-la-la(The answer is "(a - b)" not "(c - d)")Fa-la-la-la-la"
+    // @@ example @@
+//    std::string rawDelimiters{R"(The answer is "(a - b)" not "(c - d)")"};  //==> Uncomment ans see error!
+    std::string rawCustomeDelimiters{R"Fa-la-la-la-la(The answer is "(a - b)" not "(c - d)")Fa-la-la-la-la"};
+    std::cout<<rawCustomeDelimiters<<std::endl;
     return 0;
 }
